0ea34a63dd4337109d3b21372a791092b99228be
Jawed <hsjawed@yahoo.com>
2023-01-21 10:47:01 +0600

    Initial Commit

---
+++ LearningApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by MD HUSSAIN SHAH JAWED on 21/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by MD HUSSAIN SHAH JAWED on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
